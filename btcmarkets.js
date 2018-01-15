8ffb635133a1edd695f88959c238c7e9ed127af2
TaylorBett <taylor.bett@hotmail.com>
2018-01-15 11:04:57 +1100

    Initial commit

---
+++ btcmarkets.js
@@ -0,0 +1,74 @@
+const https = require('https');
+const moment = require('moment');
+
+const config = require('./config');
+
+let counter = 0;
+let collectedResults = [];
+let requestSentAt;
+
+const urlBase = 'api.btcmarkets.net';
+
+// Reset the globally scoped variables.
+reset = () => {
+    counter = 0;
+    collectedResults = [];
+    requestSentAt;
+}
+
+// Logging the data into the console. Change here if you want to customise your readout colours or format.
+logData = () => {
+    console.log('===================')
+    console.log(requestSentAt.format('HH:mm:ss, DD MMM'));
+    console.log('===================')
+    for (let i = 0; i < config.currencies.length; i += 1) {
+        let textColor = collectedResults[i].bestBid > collectedResults[i].lastPrice ? config.colors.positive : config.colors.negative;
+        if (collectedResults[i].bestBid === collectedResults[i].lastPrice) {
+            textColor = config.colors.neutral;
+        }
+        console.log(collectedResults[i].instrument + '/' + collectedResults[i].currency, config.colors.accent, (collectedResults[i].bestBid * config.currencies[i].holding).toFixed(config.currencies[i].decimals || 2), '\x1b[0m');
+        console.log('BID ', textColor, collectedResults[i].bestBid.toFixed(config.currencies[i].decimals || 2), '\x1b[0m');
+        console.log('ASK ', textColor, collectedResults[i].bestAsk.toFixed(config.currencies[i].decimals || 2), '\x1b[0m');
+        console.log('-------------------');
+    }
+    reset();
+}
+
+// Update count and check to see if it is ready to log
+updateCount = () => {
+    counter += 1;
+    if (counter === config.currencies.length) {
+        logData();
+    }
+}
+
+// Fetch latest data for particular currency/conversion pair
+getLatest = (position) => {
+    if (position === 0) {
+        requestSentAt = moment();
+    }
+    return https.get({
+        host: urlBase,
+        path: `/market/${config.currencies[position].currency}/${config.currencies[position].conversion}/tick`
+    }, (response) => {
+        var body = '';
+        response.on('data', (d) => {
+            body += d;
+        });
+        response.on('end', () => {
+            var parsed = JSON.parse(body);
+            collectedResults[position] = parsed;
+            updateCount();
+        });
+    });
+}
+
+// Fetch latest for all currencies in config array
+queryAllCurrencies = () => {
+    for (let i = 0; i < config.currencies.length; i += 1) {
+        getLatest(i);
+    }
+}
+
+queryAllCurrencies(); // For instant gratification
+setInterval(queryAllCurrencies, 20000); // Scheduled query
---
+++ config.js
@@ -0,0 +1,38 @@
+// Add currencies that you are interested in here.
+
+// If you add a value to 'holding' it will be used to
+// provide your estimated wallet value in green next to BID/ASK
+// by multiplying holding x bestBid
+
+// Providing a 'decimals' integer will control the number of 
+// decimals you will force outputs to. Defaults to 2.
+
+exports.currencies = [
+        {
+            currency: 'XRP',
+            conversion: 'AUD',
+            holding: 0,
+            decimals: 2,
+        },
+        {
+            currency: 'BTC',
+            conversion: 'AUD',
+            holding: 0,
+            decimals: 2,
+        },
+        {
+            currency: 'ETH',
+            conversion: 'BTC',
+            holding: 0,
+            decimals: 2,
+        }
+    ];
+
+// Change colors for your readout
+// Go to https://stackoverflow.com/a/41407246 for some quick options
+exports.colors = {
+    positive: '\x1b[32m',
+    neutral: '\x1b[33m',
+    negative: '\x1b[31m',
+    accent: '\x1b[36m',
+}
